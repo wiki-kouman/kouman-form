1f28a8b74272ff233562e52adddaa9be1f6b2131
Samuel Gouré <gmarcsamuel@gmail.com>
2023-07-05 14:36:33 +0000

    ajout de quelques parametres sur le formulaire

--- public/js/index.js
+++ public/js/index.js
@@ -16,18 +16,19 @@ function showTab(n) {
   } else {
     document.getElementById("nextBtn").innerHTML = "Suivant";
   }
-  if(n === x.length - 1) {
+  if (n === x.length - 1) {
     document.getElementById("nextBtn").style.display = "none";
     document.getElementById("prevBtn").style.display = "none";
-    document.getElementsByClassName("count_container")[0].style.display = "none";
+    document.getElementsByClassName("count_container")[0].style.display =
+      "none";
     document.getElementById("form_title").style.textAlign = "center";
-    document.getElementById("form_subtitle").style.display = 'none';
+    document.getElementById("form_subtitle").style.display = "none";
   }
 
   let counter_total = document.getElementById("counter_total");
   let current_counter = document.getElementById("current_counter");
-  current_counter.innerText = n +1;
-  counter_total.innerText = x.length -1;
+  current_counter.innerText = n + 1;
+  counter_total.innerText = x.length - 1;
 }
 
 function nextPrev(n) {
@@ -70,19 +71,76 @@ function validateForm() {
   return valid; // return the valid status
 }
 
+// Packs animation of Kouman
+
 const packOne = document.querySelector(".pack-one");
 const packTwo = document.querySelector(".pack-two");
 const packs = document.getElementsByClassName("pack");
-function packAnimation(n){
+function packAnimation(n) {
   const packs = document.getElementsByClassName("pack");
-  for (let i = 0; i < packs.length; i++){
+  for (let i = 0; i < packs.length; i++) {
     if (i === n) {
       packs[i].className += " active";
-      packs[i].querySelector("p").style.display = 'block';
-    }
-    else{
+      packs[i].querySelector("p").style.display = "block";
+    } else {
       packs[i].classList.remove("active");
-      packs[i].querySelector("p").style.display = 'none';
+      packs[i].querySelector("p").style.display = "none";
     }
   }
 }
+
+// Validation des differents formulaires
+
+// form 1
+
+// Récupère la référence du champ de saisie
+var inputField = document.getElementById("monChamp");
+
+// Ajoute un écouteur d'événement pour l'événement keydown
+inputField.addEventListener("keydown", function (event) {
+  // Expression régulière pour vérifier si la touche est un chiffre ou un caractère spécial
+  var regex = /[0-9!@#$%^&*()µ^¨_§£+=[\]{};':"\\|,.<>/?~`-]/;
+
+  // Vérifie le code de la touche pressée et si elle est un chiffre ou un caractère spécial
+  if (regex.test(event.key)) {
+    // Annule l'événement si la touche est un chiffre ou un caractère spécial
+    event.preventDefault();
+
+    // Affiche un message d'erreur
+    //   var errorMessage = document.getElementById("errorMessage");
+    //   errorMessage.innerHTML =
+    //     "Les chiffres et les caractères spéciaux ne sont pas autorisés.";
+    // } else {
+    // Efface le message d'erreur s'il y en avait un précédemment affiché
+    // var errorMessage = document.getElementById("errorMessage");
+    // errorMessage.innerHTML = "";
+  }
+});
+
+// Form 2
+
+// Récupère la référence du champ de saisie
+
+// Récupère la référence du champ de saisie
+var inputField = document.getElementById("monChampTwo");
+
+// Ajoute un écouteur d'événement pour l'événement keydown
+inputField.addEventListener("keydown", function (event) {
+  // Expression régulière pour vérifier si la touche est une lettre ou un caractère spécial
+  var regex = /[a-zA-Z!@#$%^&*()_£*°=[\]{};':"\\|,.<>/?~`]/;
+
+  // Vérifie le code de la touche pressée et si elle est une lettre ou un caractère spécial
+  if (regex.test(event.key)) {
+    // Annule l'événement si la touche est une lettre ou un caractère spécial
+    event.preventDefault();
+
+    // Affiche un message d'erreur
+    var errorMessage = document.getElementById("errorMessage");
+    errorMessage.innerHTML =
+      "Les lettres et les caractères spéciaux ne sont pas autorisés.";
+  } else {
+    // Efface le message d'erreur s'il y en avait un précédemment affiché
+    var errorMessage = document.getElementById("errorMessage");
+    errorMessage.innerHTML = "";
+  }
+});
